a2ce4f7e2cb08be729aeed32323da67980eceb22
Rachel Harris <raharri@microsoft.com>
2019-01-22 17:36:17 -0800

    layout fixes

--- src/components/Dealbreakers.jsx
+++ src/components/Dealbreakers.jsx
@@ -7,36 +7,45 @@ export default class Dealbreakers extends Component {
             <div className="section-panel">
                 <h3>Will you still enjoy a game where your favorite team loses?</h3>
                 <fieldset>
-                    <label>
-                        Yes
-                        <input 
-                            type="radio"
-                            checked={this.props.gameProps & DealbreakerFlags.anyLosses}
-                            onChange={(e) => this.props.setPropState(this.props.gameProps 
-                                    & ~DealbreakerFlags.fewLosses 
-                                    & ~DealbreakerFlags.noLosses
-                                    | DealbreakerFlags.anyLosses)} />
-                    </label>
-                    <label>
-                        No
-                        <input 
-                            type="radio" 
-                            checked={this.props.gameProps & DealbreakerFlags.noLosses} 
-                            onChange={(e) => this.props.setPropState(this.props.gameProps
-                                    & ~DealbreakerFlags.anyLosses
-                                    & ~DealbreakerFlags.fewLosses
-                                    | DealbreakerFlags.noLosses)} />
-                    </label>
-                    <label>
-                        Only sometimes
-                        <input 
-                            type="radio"
-                            checked={this.props.gameProps & DealbreakerFlags.fewLosses}
-                            onChange={(e) => this.props.setPropState(this.props.gameProps
-                                    & ~DealbreakerFlags.anyLosses
-                                    & ~DealbreakerFlags.noLosses
-                                    | DealbreakerFlags.fewLosses)}/>
-                    </label>
+                    <div className="game-attribute-question">
+                        <label for="any-losses">
+                            <input 
+                                id="any-losses"
+                                type="radio"
+                                checked={this.props.gameProps & DealbreakerFlags.anyLosses}
+                                onChange={(e) => this.props.setPropState(this.props.gameProps 
+                                        & ~DealbreakerFlags.fewLosses 
+                                        & ~DealbreakerFlags.noLosses
+                                        | DealbreakerFlags.anyLosses)} />
+                            Yes
+                        </label>
+                    </div>
+                    <div className="game-attribute-question">
+                        <label for="no-losses">
+                            <input 
+                                id="no-losses"
+                                type="radio" 
+                                checked={this.props.gameProps & DealbreakerFlags.noLosses} 
+                                onChange={(e) => this.props.setPropState(this.props.gameProps
+                                        & ~DealbreakerFlags.anyLosses
+                                        & ~DealbreakerFlags.fewLosses
+                                        | DealbreakerFlags.noLosses)} />
+                            No
+                        </label>
+                    </div>
+                    <div className="game-attribute-question">
+                        <label for="few-losses">
+                            <input 
+                                id="few-losses"
+                                type="radio"
+                                checked={this.props.gameProps & DealbreakerFlags.fewLosses}
+                                onChange={(e) => this.props.setPropState(this.props.gameProps
+                                        & ~DealbreakerFlags.anyLosses
+                                        & ~DealbreakerFlags.noLosses
+                                        | DealbreakerFlags.fewLosses)}/>
+                            Only sometimes
+                        </label>
+                    </div>
                 </fieldset>
             </div>
         );
--- src/components/GameFeatures.jsx
+++ src/components/GameFeatures.jsx
@@ -8,39 +8,43 @@ export default class GameFeatures extends Component {
                 <div className="answer-panel">
                     <h3>What do you like to see happen in baseball games?</h3>
                     <div className="game-attribute-question">
-                        <label>
-                            A big blowout win for my team
+                        <label for="blowout">
                             <input 
+                                id="blowout"
                                 type="checkbox"
                                 checked={this.props.gameProps & GameFlags.blowout}
                                 onChange={(e) => this.props.setPropState(e.target.checked ? this.props.gameProps | GameFlags.blowout : this.props.gameProps & ~GameFlags.blowout)} />
+                            A big blowout win for my team
                         </label>
                     </div>
                     <div className="game-attribute-question">
-                        <label>
-                            A game where both teams score a lot of runs
+                        <label for="high-scoring">
                             <input 
+                                id="high-scoring"
                                 type="checkbox"
                                 checked={this.props.gameProps & GameFlags.highScoring}
                                 onChange={(e) => this.props.setPropState(e.target.checked ? this.props.gameProps | GameFlags.highScoring : this.props.gameProps & ~GameFlags.highScoring)} />
+                            A game where both teams score a lot of runs
                         </label>
                     </div>
                     <div className="game-attribute-question">
-                        <label>
-                            One team coming from behind to win
+                        <label for="comeback">
                             <input 
+                                id="comeback"
                                 type="checkbox"
                                 checked={this.props.gameProps & GameFlags.comeback}
                                 onChange={(e) => this.props.setPropState(e.target.checked ? this.props.gameProps | GameFlags.comeback : this.props.gameProps & ~GameFlags.comeback)} />
+                            One team coming from behind to win
                         </label>
                     </div>
                     <div className="game-attribute-question" onClick={(e) => this.setState({ pitching: true })}>
-                        <label>
-                            A close pitcher's duel
+                        <label for="pitching">
                             <input 
+                                id="pitching"
                                 type="checkbox"
                                 checked={this.props.gameProps & GameFlags.pitching}
                                 onChange={(e) => this.props.setPropState(e.target.checked ? this.props.gameProps | GameFlags.pitching : this.props.gameProps & ~GameFlags.pitching)} />
+                            A close pitcher's duel
                         </label>
                     </div>
                 </div>
